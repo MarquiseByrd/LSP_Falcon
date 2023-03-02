3c0eb87be3c5814fb5c36f4d11dd262140d5d94e
Jay Byrd <Jay Byrd@DESKTOP-LR4LFB0>
2023-03-02 17:54:07 -0500

    Assignment #4

---
+++ lsp-spring23/src/org/howard/edu/lsp/assignment4/implementation/IntegerSetException.java
@@ -0,0 +1,13 @@
+package org.howard.edu.lsp.assignment4.implementation;
+/**
+ * Date: March 1-2022
+ * Error Handling for IntergerSet Class
+ * @author Marquise Byrd
+ * @version 1.0
+ *
+ */
+public class IntegerSetException extends Exception {
+	  IntegerSetException(String str){
+	    super(str);
+	  }
+}
